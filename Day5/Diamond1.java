c65dd31e7550e282ae3952845cb6eb8228dba54c
Kshitija Mohokar <68177921+kshitijamohokar@users.noreply.github.com>
2020-08-20 14:45:46 +0530

    Create Diamond1.java

---
+++ Day5/Diamond1.java
@@ -0,0 +1,38 @@
+import java.util.*; 
+ class Diamond1
+{
+static void halfDiamondStar(int N) 
+{ 
+  int i, j;
+
+    for (i = 0; i < N; i++) 
+
+    { 
+
+        for (j = 0; j < i + 1; j++) 
+
+             System.out.print("*"); 
+
+        System.out.print("\n"); 
+
+    } 
+
+    for (i = 1; i < N; i++)  
+
+    { 
+
+        for (j = i; j < N; j++) 
+
+             System.out.print("*"); 
+
+        System.out.print("\n"); 
+
+    } 
+} 
+public static void main(String[] args) 
+{ 
+
+    int N = 5;
+    halfDiamondStar(N); 
+} 
+} 
