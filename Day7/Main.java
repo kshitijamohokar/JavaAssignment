00645f8fc01ff12356c0267fb5483ccd3ebb177f
Kshitija Mohokar <68177921+kshitijamohokar@users.noreply.github.com>
2020-08-25 17:44:18 +0530

    Add files via upload

---
+++ Day7/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		Parrot parrotobj = new Parrot();
+		parrotobj.sound();
+		parrotobj.fly();
+		
+		Ostrich ostrichobj = new Ostrich();
+		ostrichobj.sound();
+		
+		Penguin penguinobj = new Penguin();
+		penguinobj.sound();
+
+		}
+}
