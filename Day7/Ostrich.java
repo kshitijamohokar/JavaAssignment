f8eb26ab9593b5f1d8891562ebb16c48787d90a6
Kshitija Mohokar <68177921+kshitijamohokar@users.noreply.github.com>
2020-08-25 17:42:49 +0530

    Add files via upload

---
+++ Day7/Ostrich.java
@@ -0,0 +1,11 @@
+public class Ostrich extends Bird {
+	
+	public void sound() {
+		System.out.println("hissing...");
+	}
+	
+	public void fly() {
+		System.out.println("Ostrich cannot fly");
+	}
+
+}
